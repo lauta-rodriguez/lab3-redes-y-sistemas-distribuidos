003d76b0191f03395869107e42424508ae5022d3
lara.kurtz <lara.kurtz@mi.unc.edu.ar>
2023-05-15 16:49:51 -0300

    feat(data.py): modify module to extract data from scalars instead of vectors

--- mediciones/data.py
+++ mediciones/data.py
@@ -1,6 +1,11 @@
+param = 'config' # key for the simulation parameters
+scalar = 'scalars' # key for the scalars array
+idx_delivered_packets = 7 # index of the delivered packets in the scalars json file
+idx_avg_delay = 5 # index of the average delay in the scalars json file
+
 def get_gen_interval(data, sim):
     # simulation parameters are stored under the key 'config'
-    gen_interval_dict = data[sim]['config'][2]
+    gen_interval_dict = data[sim][param][2]
 
     # get the generation interval (carga ofrecida) for this simulation
     gen_interval = gen_interval_dict['Network.nodeTx.gen.generationInterval']
@@ -11,15 +16,18 @@ def get_gen_interval(data, sim):
 
 
 def get_sim_time(data, sim):
-    sim_time_dict = data[sim]['config'][1]
+    sim_time_dict = data[sim][param][1]
     sim_time = sim_time_dict['sim-time-limit']
     sim_time = float(sim_time.split('s')[0])
 
     return sim_time
 
 
-def get_delivered_packets(data, sim):
-    return data[sim]['vectors'][5]['value'][-1]
+def get_avg_delivered(data, sim, sim_time):
+    total_delivered = data[sim][scalar][idx_delivered_packets]['value']
+    #print(f"total_delivered: {total_delivered}")
+    return total_delivered/sim_time
 
-def get_delay(data, sim):
-    return data[sim]['vectors'][6]['value'][-1]
+
+def get_avg_delay(data, sim):
+    return data[sim][scalar][idx_avg_delay]['value']
