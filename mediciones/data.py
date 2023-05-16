7e0d6c2962eee38e6909c5fcf6ce88cc88f6a17e
lara.kurtz <lara.kurtz@mi.unc.edu.ar>
2023-05-16 20:25:06 -0300

    feat(data): add functions to generate buffer size vs time graphs

--- mediciones/data.py
+++ mediciones/data.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 param = 'config' # key for the simulation parameters
 scalar = 'scalars' # key for the scalars array
+vector = 'vectors'
 idx_delivered_packets = 7 # index of the delivered packets in the scalars json file
 idx_avg_delay = 5 # index of the average delay in the scalars json file
 
@@ -34,6 +35,31 @@ def get_avg_delivered(data, sim, sim_time):
 def get_avg_delay(data, sim):
     return data[sim][scalar][idx_avg_delay]['value']
 
+def get_vector_time(data, sim, module):
+    idx_module = None;
+
+    if module == 'tx':
+        idx_module = 0;
+    elif module == 'subnet':
+        idx_module = 1;
+    elif module == 'rx':
+        idx_module = 2;
+
+    return data[sim][vector][idx_module]['time']
+
+def get_vector_size(data, sim, module):
+    idx_module = None;
+
+    if module == 'tx':
+        idx_module = 0;
+    elif module == 'subnet':
+        idx_module = 1;
+    elif module == 'rx':
+        idx_module = 2;
+
+    return data[sim][vector][idx_module]['value']
+
+# GRAPHS
 def ofrecida_vs_util(carga_ofrecida, carga_util, p, c):
     plt.plot(carga_ofrecida, carga_util)
 
@@ -56,4 +82,21 @@ def ofrecida_vs_retardo(carga_ofrecida, retraso,  p, c):
     plt.ylim(0, 50)
 
     plt.savefig(f"p{p}-c{c}-retardo.png")
+    plt.clf()
+
+def time_vs_bufferSize(t_tx, b_tx, t_sn, b_sn, t_rx, b_rx, p, c):
+    plt.plot(t_tx, b_tx, label='Tx', color='red')
+
+    plt.xlabel('Tiempo (seg)')
+    plt.ylabel('Tamaño del buffer (paquetes)')
+
+    plt.xlim(0, 200)
+    plt.ylim(0, 200)
+
+    plt.plot(t_sn, b_sn, label='Subnet', color='green')
+    plt.plot(t_rx, b_rx, label='Rx', color='blue')
+
+    plt.legend()
+
+    plt.savefig(f"p{p}-c{c}-bufferSize.png")
     plt.clf()
