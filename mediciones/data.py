ce008d6895f0a29a41b7e3b9dadc0164ace0e042
lara.kurtz <lara.kurtz@mi.unc.edu.ar>
2023-05-17 01:59:08 -0300

    feat: reimplemented both files almost entirely because they were not working correctly

--- mediciones/data.py
+++ mediciones/data.py
@@ -1,44 +1,81 @@
+import os
+import json
 import matplotlib.pyplot as plt
 
 param = 'config'  # key for the simulation parameters
 scalar = 'scalars'  # key for the scalars array
 vector = 'vectors'
-#idx_delivered_packets = 7  # index of the delivered packets in the scalars json file
-#idx_avg_delay = 5  # index of the average delay in the scalars json file
-idx_gen_interval = 3
+
+# DATA extractor
+def get_scalar_data(p,c):
+    carga_ofrecida = [0]
+    carga_util = [0]
+    retraso = [0]
+
+    for i in range(5, 0, -1):
+        fscalar = f"scalar-p{p}-c{c}-0{i}.json"
+        print(fscalar)
+
+        if os.path.isfile(fscalar):
+            # open the json file
+            with open(fscalar) as f:
+                data = json.load(f)
+
+            # the simulation is stored in the first key of the dictionary
+            sim = list(data.keys())[0]
+            # simulation parameters
+            sim_time = get_sim_time(data, sim)
+            # simulation results
+            avg_sent = get_avg_sent(data, sim, sim_time)
+            avg_delivered = get_avg_delivered(data, sim, sim_time)
+            avg_delay = get_avg_delay(data, sim)
+            # append value to each axis data
+            carga_ofrecida.append(float(avg_sent))
+            carga_util.append(float(avg_delivered))
+            retraso.append(float(avg_delay))
+
+        else:  # there aren't any more simulations for this parte/caso
+            break
+
+    return (carga_ofrecida, carga_util, retraso)
+
+def get_vector_data(p,c):
+    tx_time = [0]
+    tx_size = [0]
+    subnet_time = [0]
+    subnet_size = [0]
+    rx_time = [0]
+    rx_size = [0]
+
+    for i in range(1, 5):
+        fvector = f"vector-p{p}-c{c}-0{i}.json"
+        if os.path.isfile(fvector):
+            # open the json file
+            with open(fvector) as g:
+                data = json.load(g)
+
+            # the simulation is stored in the first key of the dictionary
+            sim = list(data.keys())[0]
+
+            tx_time = get_vector_time(data, sim, 'tx')
+            tx_size = get_vector_size(data, sim, 'tx')
+            subnet_time = get_vector_time(data, sim, 'subnet')
+            subnet_size = get_vector_size(data, sim, 'subnet')
+            rx_time = get_vector_time(data, sim, 'rx')
+            rx_size = get_vector_size(data, sim, 'rx')
+
+        else:  # there aren't any more simulations for this parte/caso
+            break
+
+    return (tx_time, tx_size, subnet_time, subnet_size, rx_time, rx_size)
 
 # JSON HELPERS
-def get_index_of(name, scalars):
+def get_index_of_scalar(name, scalars):
     for index, scalar in enumerate(scalars):
         if scalar["name"] == name:
             return index
 
-#def get_index_of(name, config):
-#    for index, config in enumerate(config):
-#        if scalar[config] == name:
-#            return index
-
 # DATA GETTERS
-def get_gen_interval(data, sim, p):
-    # simulation parameters are stored under the key 'config'
-    #config_dict = data[sim][param]
-
-    if (p == 1):
-        id = 2
-        key_gen = 'Network.nodeTx.gen.generationInterval'
-    else:
-        id = 3
-        key_gen = 'Network.Transmitter.gen.generationInterval'
-
-    # get the generation interval (carga ofrecida) for this simulation
-    gen_interval = data[sim]['config'][id][key_gen]
-    print(f"gen_interval: {gen_interval}")
-    # get the number value from the key value
-    gen_interval = float(gen_interval.split('(')[1].split(')')[0])
-
-    return gen_interval
-
-
 def get_sim_time(data, sim):
     sim_time_dict = data[sim][param][1]
     sim_time = sim_time_dict['sim-time-limit']
@@ -46,18 +83,23 @@ def get_sim_time(data, sim):
 
     return sim_time
 
+def get_avg_sent(data, sim, sim_time):
+    idx_sent_packets = get_index_of_scalar('sent packets', data[sim][scalar])
+    total_sent = data[sim][scalar][idx_sent_packets]['value']
+
+    return total_sent/sim_time
 
 def get_avg_delivered(data, sim, sim_time):
-    scalar_array = data[sim][scalar]  # [idx_delivered_packets]['value']
-    # print(f"total_delivered: {total_delivered}")
-    idx_delivered_packets = get_index_of('Delivered packets', scalar_array)
+    scalar_array = data[sim][scalar]
+
+    idx_delivered_packets = get_index_of_scalar('delivered packets', scalar_array)
     total_delivered = scalar_array[idx_delivered_packets]['value']
-    #print(f"idx_delivered_packets: {idx_delivered_packets}")
+
     return total_delivered/sim_time
 
 def get_avg_delay(data, sim):
-    idx_avg_delay = get_index_of('Avg delay', data[sim][scalar])
-    #print(f"idx_avg_delay: {idx_avg_delay}")
+    idx_avg_delay = get_index_of_scalar('average delay', data[sim][scalar])
+
     return data[sim][scalar][idx_avg_delay]['value']
 
 
@@ -85,49 +127,3 @@ def get_vector_size(data, sim, module):
         idx_module = 2
 
     return data[sim][vector][idx_module]['value']
-
-# GRAPHS
-
-
-def ofrecida_vs_util(carga_ofrecida, carga_util, p, c):
-    plt.plot(carga_ofrecida, carga_util)
-
-    plt.xlabel('Carga ofrecida (paquetes/seg)')
-    plt.ylabel('Carga útil (paquetes/seg)')
-
-    plt.xlim(0, 1)
-    plt.ylim(0, 50)
-
-    plt.savefig(f"p{p}-c{c}-util.png")
-    plt.clf()
-
-
-def ofrecida_vs_retardo(carga_ofrecida, retraso,  p, c):
-    plt.plot(carga_ofrecida, retraso)
-
-    plt.xlabel('Carga ofrecida (paquetes/seg)')
-    plt.ylabel('Retardo (seg)')
-
-    plt.xlim(0, 1)
-    plt.ylim(0, 50)
-
-    plt.savefig(f"p{p}-c{c}-retardo.png")
-    plt.clf()
-
-
-def time_vs_bufferSize(t_tx, b_tx, t_sn, b_sn, t_rx, b_rx, p, c):
-    plt.plot(t_tx, b_tx, label='Tx', color='red')
-
-    plt.xlabel('Tiempo (seg)')
-    plt.ylabel('Tamaño del buffer (paquetes)')
-
-    plt.xlim(0, 200)
-    plt.ylim(0, 200)
-
-    plt.plot(t_sn, b_sn, label='Subnet', color='green')
-    plt.plot(t_rx, b_rx, label='Rx', color='blue')
-
-    plt.legend()
-
-    plt.savefig(f"p{p}-c{c}-bufferSize.png")
-    plt.clf()
--- mediciones/plotting.py
+++ mediciones/plotting.py
@@ -1,87 +1,52 @@
-# argument manipulation
-import argparse
-# file manipulation
-import os
-import glob
-# data parsing
-import json
 from data import *
 
-# create an ArgumentParser object
-parser = argparse.ArgumentParser(
-    usage='%(prog)s --parte=<1,2> --caso=<1,2>'
-)
+#carga_ofrecida_p2c1, carga_util_p2c1, retraso_p2c1 = get_scalar_data(2, 1)
+#carga_ofrecida_p2c2, carga_util_p2c2, retraso_p2c2 = get_scalar_data(2, 2)
+#
+#xlim = max(max(carga_ofrecida_p2c1),max(carga_ofrecida_p2c2))
+#ylim = max(max(carga_util_p2c1),max(carga_util_p2c2))
+#
+#plt.plot(carga_ofrecida_p2c1, carga_util_p2c1, label='caso 1')
+#plt.plot(carga_ofrecida_p2c2, carga_util_p2c2, label='caso 2')
+#
+#plt.xlabel('Carga ofrecida (paquetes/seg)')
+#plt.ylabel('Carga útil (paquetes/seg)')
+#
+#plt.xlim(0, xlim)
+#plt.ylim(0, ylim)
+#
+#plt.legend(loc='upper left')
+#
+#plt.savefig('ofrecida_vs_util_caso1.png')
+#
+#plt.clf()
+#
+#xlim = max(max(carga_ofrecida_p2c1),max(carga_ofrecida_p2c2))
+#ylim = max(max(retraso_p2c1),max(retraso_p2c2))
+#
+#plt.plot(carga_ofrecida_p2c1, retraso_p2c1, label='caso 1')
+#plt.plot(carga_ofrecida_p2c2, retraso_p2c2, label='caso 2')
+#
+#plt.xlabel('Carga ofrecida (paquetes/seg)')
+#plt.ylabel('Retardo (seg)')
+#
+#plt.xlim(0, xlim)
+#plt.ylim(0, ylim)
+#
+#plt.legend(loc='upper left')
+#plt.savefig('ofrecida_vs_retardo_caso1.png')
+#
+#plt.clf()
 
-# add the arguments
-parser.add_argument('--parte', type=int, help='an integer for parte')
-parser.add_argument('--caso', type=int, help='an integer for caso')
+tx_time_p2c1, tx_size_p2c1, subnet_time_p2c1, subnet_size_p2c1, rx_time_p2c1, rx_size_p2c1 = get_vector_data(2, 1)
+tx_time_p2c2, tx_size_p2c2, subnet_time_p2c2, subnet_size_p2c2, rx_time_p2c2, rx_size_p2c2 = get_vector_data(2, 2)
 
-# parse the arguments
-args = parser.parse_args()
+plt.plot(tx_time_p2c1, tx_size_p2c1, label='tx')
+plt.plot(subnet_time_p2c1, subnet_size_p2c1, label='subnet')
+plt.plot(rx_time_p2c1, rx_size_p2c1, label='rx')
 
-# check if arguments are valid
-if args.parte is None or args.caso is None:
-    parser.print_usage()
-    exit()
+plt.xlabel('Tiempo (seg)')
+plt.ylabel('Ocupación del buffer')
+plt.legend(loc='upper left')
 
-# store the arguments in variables
-p = args.parte
-c = args.caso
-
-# create list for each axis of both plots
-carga_ofrecida = [0]
-carga_util = [0]
-retraso = [0]
-
-# iterate over all simulation scalar results for this parte/caso
-for i in range(1, 20):
-    fscalar = f"scalar-p{p}-c{c}-0{i}.json"
-    fvector = f"vector-p{p}-c{c}-0{i}.json"
-
-    if os.path.isfile(fscalar):
-        # open the json file
-        with open(fscalar) as f:
-            data = json.load(f)
-
-        # the simulation is stored in the first key of the dictionary
-        sim = list(data.keys())[0]
-
-        # simulation parameters
-        sim_time = get_sim_time(data, sim)
-        gen_interval = get_gen_interval(data, sim, p)
-
-        # simulation results
-        avg_delivered = get_avg_delivered(data, sim, sim_time)
-        #print(f"avg_delivered: {avg_delivered}")
-        avg_delay = get_avg_delay(data, sim)
-        #print(f"avg_delay: {avg_delay}")
-
-        # append value to each axis data
-        carga_ofrecida.append(gen_interval)
-        carga_util.append(avg_delivered)
-        retraso.append(avg_delay)
-
-    else:  # there aren't any more simulations for this parte/caso
-        break
-
-    if os.path.isfile(fvector):
-        # open the json file
-        with open(fvector) as g:
-            data = json.load(g)
-
-        # the simulation is stored in the first key of the dictionary
-        sim = list(data.keys())[0]
-
-        tx_time = get_vector_time(data, sim, 'tx')
-        tx_size = get_vector_size(data, sim, 'tx')
-        subnet_time = get_vector_time(data, sim, 'subnet')
-        subnet_size = get_vector_size(data, sim, 'subnet')
-        rx_time = get_vector_time(data, sim, 'rx')
-        rx_size = get_vector_size(data, sim, 'rx')
-
-    else:  # there aren't any more simulations for this parte/caso
-        break
-
-ofrecida_vs_util(carga_ofrecida, carga_util, p, c)
-ofrecida_vs_retardo(carga_ofrecida, retraso, p, c)
-time_vs_bufferSize(tx_time, tx_size, subnet_time, subnet_size, rx_time, rx_size, p, c)
+plt.show()
