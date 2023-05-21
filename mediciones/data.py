55b18e55a0fb0bc02a07c4a57f1d5311bbb8ee35
lara.kurtz <lara.kurtz@mi.unc.edu.ar>
2023-05-21 13:55:34 -0300

    feat: reimplemente the plotting program once again. Now is legible

--- mediciones/data.py
+++ mediciones/data.py
@@ -1,129 +1,53 @@
+# here will go helper functions for plot.py that will get the required information
+# for a specific plot from the json files
 import os
 import json
 import matplotlib.pyplot as plt
 
-param = 'config'  # key for the simulation parameters
-scalar = 'scalars'  # key for the scalars array
-vector = 'vectors'
+FILE_NAME_FORMAT = './data-samples/{type}-p{scenario}-c{case}-s{iteration}.json'
+GRAPH_DIR = './graphs'
 
-# DATA extractor
-def get_scalar_data(p,c):
-    carga_ofrecida = [0]
-    carga_util = [0]
-    retraso = [0]
-
-    for i in range(5, 0, -1):
-        fscalar = f"scalar-p{p}-c{c}-0{i}.json"
-        print(fscalar)
-
-        if os.path.isfile(fscalar):
-            # open the json file
-            with open(fscalar) as f:
-                data = json.load(f)
-
-            # the simulation is stored in the first key of the dictionary
-            sim = list(data.keys())[0]
-            # simulation parameters
-            sim_time = get_sim_time(data, sim)
-            # simulation results
-            avg_sent = get_avg_sent(data, sim, sim_time)
-            avg_delivered = get_avg_delivered(data, sim, sim_time)
-            avg_delay = get_avg_delay(data, sim)
-            # append value to each axis data
-            carga_ofrecida.append(float(avg_sent))
-            carga_util.append(float(avg_delivered))
-            retraso.append(float(avg_delay))
-
-        else:  # there aren't any more simulations for this parte/caso
-            break
-
-    return (carga_ofrecida, carga_util, retraso)
-
-def get_vector_data(p,c):
-    tx_time = [0]
-    tx_size = [0]
-    subnet_time = [0]
-    subnet_size = [0]
-    rx_time = [0]
-    rx_size = [0]
-
-    for i in range(1, 5):
-        fvector = f"vector-p{p}-c{c}-0{i}.json"
-        if os.path.isfile(fvector):
-            # open the json file
-            with open(fvector) as g:
-                data = json.load(g)
-
-            # the simulation is stored in the first key of the dictionary
-            sim = list(data.keys())[0]
-
-            tx_time = get_vector_time(data, sim, 'tx')
-            tx_size = get_vector_size(data, sim, 'tx')
-            subnet_time = get_vector_time(data, sim, 'subnet')
-            subnet_size = get_vector_size(data, sim, 'subnet')
-            rx_time = get_vector_time(data, sim, 'rx')
-            rx_size = get_vector_size(data, sim, 'rx')
-
-        else:  # there aren't any more simulations for this parte/caso
-            break
-
-    return (tx_time, tx_size, subnet_time, subnet_size, rx_time, rx_size)
-
-# JSON HELPERS
-def get_index_of_scalar(name, scalars):
-    for index, scalar in enumerate(scalars):
-        if scalar["name"] == name:
+# helper function. Returns the index of the vector with the given module and name
+def get_index(dict, module, name, data_type):
+    for index, element in enumerate(dict[data_type]):
+        if element['module'] == module and element['name'] == name:
             return index
+    return -1  # Return -1 if the vector is not found
 
-# DATA GETTERS
-def get_sim_time(data, sim):
-    sim_time_dict = data[sim][param][1]
-    sim_time = sim_time_dict['sim-time-limit']
-    sim_time = float(sim_time.split('s')[0])
+# returns the data of the vector/scalar with the given module, metric and name
+def get_data(module, metric, name, data_type, sim, it):
+    fvector = FILE_NAME_FORMAT.format(
+        type=data_type, scenario=sim[0], case=sim[1], iteration=it)
 
-    return sim_time
+    if os.path.isfile(fvector):
+        # open the json file
+        with open(fvector) as f:
+            data = json.load(f)
 
-def get_avg_sent(data, sim, sim_time):
-    idx_sent_packets = get_index_of_scalar('sent packets', data[sim][scalar])
-    total_sent = data[sim][scalar][idx_sent_packets]['value']
+        # the simulation data is stored in the first key of the json dictionary
+        sim_dict = data[list(data.keys())[0]]
 
-    return total_sent/sim_time
+        # get the index of the vector
+        index = get_index(sim_dict, module, metric, data_type)
 
-def get_avg_delivered(data, sim, sim_time):
-    scalar_array = data[sim][scalar]
+        return sim_dict[data_type][index][name]
 
-    idx_delivered_packets = get_index_of_scalar('delivered packets', scalar_array)
-    total_delivered = scalar_array[idx_delivered_packets]['value']
+    else:
+        print(f'File {fvector} not found.')
 
-    return total_delivered/sim_time
+    return None
 
-def get_avg_delay(data, sim):
-    idx_avg_delay = get_index_of_scalar('average delay', data[sim][scalar])
+# saves a plot with the speccified filename
+def save_plot(filename):
+    filename = GRAPH_DIR + '/' + filename
+    _, name, extension = filename.split('.')
 
-    return data[sim][scalar][idx_avg_delay]['value']
+    i = 0
+    while (os.path.isfile(f'.{name}-{i}.{extension}')):
+        i += 1
 
+    # split the filename into name and extension
+    filename = f'.{name}-{i}.{extension}'
 
-def get_vector_time(data, sim, module):
-    idx_module = None
-
-    if module == 'tx':
-        idx_module = 0
-    elif module == 'subnet':
-        idx_module = 1
-    elif module == 'rx':
-        idx_module = 2
-
-    return data[sim][vector][idx_module]['time']
-
-
-def get_vector_size(data, sim, module):
-    idx_module = None
-
-    if module == 'tx':
-        idx_module = 0
-    elif module == 'subnet':
-        idx_module = 1
-    elif module == 'rx':
-        idx_module = 2
-
-    return data[sim][vector][idx_module]['value']
+    plt.savefig(filename)
+    print(f'Figure saved as {filename}')
