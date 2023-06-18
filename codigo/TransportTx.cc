eaa86b7a0572f5e14c4ac655d1455c3cb911c4d3
lara.kurtz <lara.kurtz@mi.unc.edu.ar>
2023-06-18 20:37:08 -0300

    docs(TransportTx): better comments, specially on the penalty system con bottleneck detection

--- codigo/TransportTx.cc
+++ codigo/TransportTx.cc
@@ -21,7 +21,6 @@ private:
     cQueue buffer;
     cMessage *endServiceEvent;
     simtime_t serviceTime;
-    simtime_t BOTTLENECK_WINDOW;
 
     // variables for statistics logging
     unsigned int sentPackets;
@@ -30,14 +29,22 @@ private:
     // defines how many seconds the network is considered to be bottlenecked
     // after a feedback message is recieved
     const unsigned int TIMER = 5;
+
+    // determines a point in time until which the network is considered to be
+    // bottlenecked, calculated as simTime() + TIMER
+    simtime_t BOTTLENECK_WINDOW;
+
     // if a bottleneck occurs in less than PENALTY_WINDOW seconds since the last
-    // one, a penalty is applied to the transmitter
+    // bottleneck, a penalty is applied to the transmitter
     const unsigned int PENALTY_WINDOW = 10;
+
     // default modifier for serviceTime
     const unsigned int k = 20;
+
     // variable modifier for serviceTime
     unsigned int mod;
-    // flag for bottleneck
+
+    // flag used for indicating a bottleneck is occurring
     bool bottleneck;
 
     // helper function for handling the queuing process in the buffer
@@ -89,23 +96,32 @@ void TransportTx::finish()
 
 void TransportTx::handleMessage(cMessage *msg)
 {
-    // determine if a bottleneck has occurred
+    // if a feedback message is received and the bottleneck flag is off, then
+    // a new bottleneck has occurred
     if (msg->getKind() == 2 && !bottleneck)
     {
+        // set bottleneck flag to indicate that a bottleneck is occurring
         bottleneck = true;
-        // apply penalty to the transmitter if appropriate
+
+        // calculate penalty to be applied to the transmitter if less than
+        // PENALTY_WINDOW seconds have passed since the last bottleneck
         if (simTime() - BOTTLENECK_WINDOW < PENALTY_WINDOW)
         {
+            // increment modifier used to increase serviceTime of data packets
             mod *= 2;
         }
+
+        // set end time for bottleneck as TIMER seconds from now
         BOTTLENECK_WINDOW = simTime() + TIMER;
     }
 
-    // determine if bottleneck is over
+    // if the bottleneck window has passed, then the bottleneck is over
     if (simTime() >= BOTTLENECK_WINDOW && bottleneck)
     {
+        // reset bottleneck flag to indicate that the bottleneck is over
         bottleneck = false;
-        // reset modifier to default value
+
+        // reset modifier to default value to remove penalty for next bottleneck
         mod = k;
     }
 
@@ -126,6 +142,7 @@ void TransportTx::handleMessage(cMessage *msg)
                 // serviceTime now depends on pkt->getDuration()
                 serviceTime = pkt->getDuration();
 
+                // if a bottleneck is occurring, apply modifier to serviceTime
                 if (bottleneck)
                 {
                     serviceTime *= mod;
